278fa04bed8c5c6cc8e8f1956d3618c6d75ed995
msparmar4 <126829847+msparmar4@users.noreply.github.com>
2023-06-08 01:16:37 -0400

    Add files via upload

---
+++ Terraform/modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "App"   = "Docker-EC2"
+    "Onwer" = "msparmar"
+  }
+}
+
+# Prefix to for resources
+output "prefix" {
+  value     = "Assignment1-Clo835"
+}
